ca38fe873a7d9989eee1502a2a8f3425555f4126
GooDCactuS <Marvele98@gmail.com>
2020-07-06 16:47:55 +0400

    feat: add asp.net project (task 1)

---
+++ Stemy/ASP.NET Project/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP.NET_Project.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        IWebHostEnvironment _appEnvironment;
+
+        public HomeController(IWebHostEnvironment appEnvironment)
+        {
+            _appEnvironment = appEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserPictureModel model)
+        {
+            var file = HttpContext.Request.Form.Files[0];
+            model.FilePath = "/Images/" + HttpContext.Request.Form.Files[0].FileName;
+            byte[] pic = null;
+            using (var binaryReader = new BinaryReader(file.OpenReadStream()))
+            {
+                pic = binaryReader.ReadBytes((int) file.Length);
+            }
+
+            model.Picture = pic;
+            return View("UserPicture", model);
+        }
+    }
+}
---
+++ Stemy/ASP.NET Project/Models/UserPictureModel.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+
+namespace ASP.NET_Project.Models
+{
+    public class UserPictureModel
+    {
+        public string Description { get; set; }
+        public byte[] Picture { get; set; }
+
+        public string FilePath { get; set; }
+    }
+}
